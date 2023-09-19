07ccb8d0cc49c309072eadf6d1e4362aeafdbe39
ScoutLondon <mynewhousenc@gmail.com>
2023-09-19 14:59:35 -0400

    scanner example

--- PerScholas/src/perscholas/ScannerExample.java
+++ PerScholas/src/perscholas/ScannerExample.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 
 
 public class ScannerExample {
-
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your favorite team?");
+		String x = input.nextLine();
+		System.out.println("Go " + x + "!");
+	}
+	
 }
