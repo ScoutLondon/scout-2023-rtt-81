fca1fbe56d381954fbebe92a189e140b0c7cb73b
ScoutLondon <mynewhousenc@gmail.com>
2023-10-25 15:00:45 -0400

    Fixed "findByName" function in ProductDAO.  Added guide in comments for future help understanding "setParameter".

--- database/src/main/java/org/perscholas/database/CreateOrderDetails.java
+++ database/src/main/java/org/perscholas/database/CreateOrderDetails.java
@@ -1,5 +1,8 @@
 package org.perscholas.database;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.perscholas.database.dao.OrderDAO;
 import org.perscholas.database.dao.OrderDetailDAO;
 import org.perscholas.database.dao.ProductDAO;
@@ -15,9 +18,47 @@ public class CreateOrderDetails {
 	
 	
 	public void createOrderDetails(){
+		//ask user to enter product name
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a product name to add to your order");
+		String productName = scan.nextLine();
+		
+		//looks up product by productName
+		List<Product> products = productDAO.findByName(productName);
+		
+		if (products.size() == 0) {
+			System.out.println("You did not enter a valid product name");
+			System.exit(0);
+		}
+		
+		//Query can return more than one product with same name
+		//so we list products that were returned
+		for (Product product : products) {
+			System.out.println(product.getId() + " | " + product.getProductName());
+		}
+		
+		//of list of product names shown, allow user to select id they want to add
+		System.out.println("Select a product Id:");
+		Integer productId = scan.nextInt();
+		
+		
+		
 		//goal is to insert record into order details table
-		Product p = productDAO.findById(1);
+		
+		Product p = productDAO.findById(productId);
+		//ask use what order number they want to add product to
 		Order o = orderDAO.findById(10100);
+		
+		//if product is already part of order don't add it again
+		for (OrderDetail orderDetail : o.getOrderdetails()) {
+			if (orderDetail.getProduct().getId() == productId) {
+				//the product is already part of the order
+				System.out.println("The product "+ p.getProductName() + " is already part of the order, cannot add again.");
+				System.exit(1);
+			}
+		}
+		
+		
 		
 		OrderDetail od = new OrderDetail();
 		od.setProduct(p);
@@ -27,6 +68,8 @@ public class CreateOrderDetails {
 		od.setQuantityOrdered(300);
 		
 		orderDetailDAO.save(od);
+		
+		System.out.println("Successfully added product to order");
 		
 	}
 	
--- database/src/main/java/org/perscholas/database/dao/ProductDAO.java
+++ database/src/main/java/org/perscholas/database/dao/ProductDAO.java
@@ -1,5 +1,7 @@
 package org.perscholas.database.dao;
 
+import java.util.List;
+
 import javax.persistence.TypedQuery;
 
 import org.hibernate.Session;
@@ -19,6 +21,37 @@ public class ProductDAO {
 		query.setParameter("id", id);
 		
 		Product result = query.getSingleResult();
+		return result;
+		
+	
+	}
+	
+	//adding a findByName method so I can run CreateOrderDetails
+	//THERE IS AN ISSUE IN FINDBYNAME
+	//CANNOT RESOLVE PROPERTY: PRODUCT_NAME
+	public List<Product> findByName(String name) {
+		SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		Session session = factory.openSession();
+		
+		//ISSUE RESOLVED: p.productName as named in entity vs p.product_name
+		String hql = "FROM Product p WHERE p.productName = :name"; // Example of HQL to get all records of product class
+		
+		TypedQuery<Product> query = session.createQuery(hql, Product.class);
+		query.setParameter("name", name);
+		
+		//setParameter Guide:
+		
+		//"name": This is the parameter name you're specifying in the query. 
+		//When you use query.setParameter("name", name), you're telling Hibernate 
+		//that you want to bind the value of the Java variable name to the named 
+		//parameter "name" in the HQL query.
+
+		//name: This is the Java variable that holds the actual value you want to 
+		//use in the query. You're passing this value as an argument to the 
+		//findByName method.
+		
+		
+		List<Product> result = query.getResultList();
 		return result;
 		
 	
