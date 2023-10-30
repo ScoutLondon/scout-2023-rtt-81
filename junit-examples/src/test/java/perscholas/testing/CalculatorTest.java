78ff4e9064f3eacc4d10343d98f68af470965485
ScoutLondon <mynewhousenc@gmail.com>
2023-10-30 16:09:30 -0400

    New tests and parameterized tests for calculator

--- junit-examples/src/test/java/perscholas/testing/CalculatorTest.java
+++ junit-examples/src/test/java/perscholas/testing/CalculatorTest.java
@@ -4,23 +4,57 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalculatorTest {
 	
-	@Test
-	public void additionTest() {
+	@ParameterizedTest
+	@CsvSource({
+		"5, 5, 10",
+		"10, 10, 20",
+		"5.5, 5.5, 11.0"
+	})
+	public void additionTest(double a, double b, double answer) {
 		Calculator c = new Calculator();
-		double expected = 10.0;
 		
-		double actual = c.add(5.0, 5.0);
+		double actual = c.add(a, b);
 		
-		Assertions.assertEquals(expected, actual);
-		
-		Assertions.assertEquals(15.0, c.add(7.5, 7.5));
+		Assertions.assertEquals(answer, actual);
 	}
 	
+	@ParameterizedTest
+	@CsvSource({
+		"5, 5, 12",
+		"10, 10, 20.5",
+		"5.5, 5.5, 12.0"
+	})
+	public void additionErrorTest(double a, double b, double answer) {
+		Calculator c = new Calculator();
+		
+		double actual = c.add(a, b);
+		
+		Assertions.assertNotEquals(answer, actual);
+	}
+	
+	
+	@ParameterizedTest
+	@CsvSource({
+		"5, 5, 0",
+		"45, 10, 35",
+		"21, 7, 14"
+	})
+	public void subtractionTest(double a, double b, double answer) {
+		Calculator c = new Calculator();
+		
+		double actual = c.subtract(a, b);
+		
+		Assertions.assertEquals(answer, actual);
+	}
+	
+	
 	@Test
-	public void subtractionTest() {
+	public void nonparameterizedSubtractionTest() {
 		Calculator c = new Calculator();
 		double expected = 0.0;
 		
@@ -29,6 +63,34 @@ public class CalculatorTest {
 		Assertions.assertEquals(expected, actual);
 		
 		Assertions.assertEquals(15.0, c.subtract(20.0, 5.0));
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+		"5, 5, 25",
+		"10, 10, 100",
+		"20, 3, 60"
+	})
+	public void multiplyTest(double a, double b, double answer) {
+		Calculator c = new Calculator();
+		
+		double actual = c.multiple(a, b);
+		
+		Assertions.assertEquals(answer, actual);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+		"5, 5, 1",
+		"10, 5, 2",
+		"35, 5, 7"
+	})
+	public void divideTest(double a, double b, double answer) {
+		Calculator c = new Calculator();
+		
+		double actual = c.divide(a, b);
+		
+		Assertions.assertEquals(answer, actual);
 	}
 
 }
