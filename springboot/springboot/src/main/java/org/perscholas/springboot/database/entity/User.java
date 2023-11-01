6630ab6b0f39cf34d034b5c677d39319454027a6
ScoutLondon <mynewhousenc@gmail.com>
2023-11-01 16:50:17 -0400

    Experimenting with springboot controllers in IntelliJ

---
+++ springboot/springboot/src/main/java/org/perscholas/springboot/database/entity/User.java
@@ -0,0 +1,19 @@
+package org.perscholas.springboot.database.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "email")
+    private String email;
+}
