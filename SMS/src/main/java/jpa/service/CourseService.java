507183e6d070f298b3e431eeb7a7a2d1b7bbab28
ScoutLondon <mynewhousenc@gmail.com>
2023-11-03 01:49:36 -0400

    Created service classes implementing CourseDAO and StudentDAO -Need to write implementations

---
+++ SMS/src/main/java/jpa/service/CourseService.java
@@ -0,0 +1,9 @@
+package jpa.service;
+
+import jpa.dao.CourseDAO;
+
+//TODO: Implement methods
+
+public class CourseService implements CourseDAO {
+
+}
---
+++ SMS/src/main/java/jpa/service/StudentService.java
@@ -0,0 +1,9 @@
+package jpa.service;
+
+import jpa.dao.StudentDAO;
+
+//TODO: Implement methods
+
+public class StudentService implements StudentDAO{
+
+}
